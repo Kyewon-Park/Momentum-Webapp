a192932cb58058eb735b213f49ba7f83833661b4
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-01-22 01:02:42 +0900

    finish greeting part

--- greeting.js
+++ greeting.js
@@ -1,16 +1,17 @@
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings");
-let asf = input;
+
 const USER_LS = "currentUser", //keyName
   SHOWING_CN = "showing";
 
 function saveName(text) {
-  //text = keyValue
+  //text : keyValue
   localStorage.setItem(USER_LS, text);
 }
 
 function handleSubmit(event) {
+  //JS will give you the event object, so you can use it if you want to
   event.preventDefault(); //기본동작 막기(refresh같은)
   const currentValue = input.value; //input에 있는 값 저장
   paintGreeting(currentValue);
@@ -18,9 +19,10 @@ function handleSubmit(event) {
 }
 
 function paintForm() {
-  form.classList.add(SHOWING_CN);
-  greeting.classList.remove(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit);
+  form.classList.add(SHOWING_CN); //form을 보이게 함
+  greeting.classList.remove(SHOWING_CN); //greeting을 안보이게 함
+  form.addEventListener("submit", handleSubmit); //이벤트리스너를 실행시킨다.
+  //submit 이벤트가 일어나면 handleSubmit(event)
 }
 
 function paintGreeting(text) {
